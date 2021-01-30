bd1eabd6542b9f1f5f2887ede91326e45a7223b1
Vusal <Mmdvvsl@gmail.com>
2021-01-30 17:13:40 +0300

    Test Git

---
+++ src/ru/geekbrain/test/Main.java
@@ -0,0 +1,12 @@
+package ru.geekbrain.test;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("eswrswr");
+    }
+}
